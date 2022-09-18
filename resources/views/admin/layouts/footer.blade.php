6e58b138aa1bdc9070f5686407c9c246d834b3d7
manhhung2801 <hung28012002zz@gmail.com>
2022-09-18 18:12:52 +0700

    hung cat san layouts admin

---
+++ resources/views/admin/layouts/footer.blade.php
@@ -0,0 +1,6 @@
+<!-- jQuery -->
+<script src="{{asset('admin/assets/plugins/jquery/jquery.min.js')}}"></script>
+<!-- Bootstrap 4 -->
+<script src="{{asset('admin/assets/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<!-- AdminLTE App -->
+<script src="{{asset('admin/assets/dist/js/adminlte.min.js')}}"></script>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Admin\LoginController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,7 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::get('login-admin', [LoginController::class, 'login']);
